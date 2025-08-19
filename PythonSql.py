a2c84b46e28d78b6a49cce6a4c322eebe0b38f43
AJtcarv <arlesonc85@gmail.com>
2025-08-19 16:38:38 -0300

    PrimeiraIntegracaoComBancoDeDados

--- PythonSql.py
+++ PythonSql.py
@@ -10,4 +10,12 @@ dados_conexao = (
 conexao = pyodbc.connect(dados_conexao)
 print('Conexão Bem Sucedida')
 
-cursor = 
+cursor = conexao.cursor()
+
+comando = """INSERT INTO Vendas(id_venda, cliente, produto, data_venda, preco, quantidade) 
+VALUES
+    (4, 'camille', 'geladeira', '2025/08/11', 4500, 1),
+    (5, 'jorge', 'frigobar', '2025/08/15', 7000, 1)"""
+
+cursor.execute(comando)
+cursor.commit()
