43dfaddde4693ea88d5b8ff39ae33522ecbb26a0
AJtcarv <arlesonc85@gmail.com>
2025-08-19 16:58:59 -0300

    AutomatizandoEntradasDinamicas

--- PythonSql.py
+++ PythonSql.py
@@ -11,12 +11,12 @@ conexao = pyodbc.connect(dados_conexao)
 print('Conexão Bem Sucedida')
 
 cursor = conexao.cursor()
-id = "6"
-cliente = "joana"
-produto = "carro"
-data = "2025/12/25"
-preco = "9000"
-quantidade = "1"
+id = int(input('id: '))
+cliente = str(input("Cliente: "))
+produto = str(input('Produto: '))
+data = str(input('data: (formato yyyy-mm-dd)'))
+preco = int(input('Preço: '))
+quantidade = int(input('Quantidade: '))
 
 comando = f"""INSERT INTO Vendas(id_venda, cliente, produto, data_venda, preco, quantidade) 
 VALUES
