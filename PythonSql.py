720c214c3831c61fc46078a58753bde86be92951
AJtcarv <arlesonc85@gmail.com>
2025-08-19 16:48:33 -0300

    EntradaDinamica

--- PythonSql.py
+++ PythonSql.py
@@ -11,11 +11,16 @@ conexao = pyodbc.connect(dados_conexao)
 print('Conexão Bem Sucedida')
 
 cursor = conexao.cursor()
+id = "6"
+cliente = "joana"
+produto = "carro"
+data = "2025/12/25"
+preco = "9000"
+quantidade = "1"
 
-comando = """INSERT INTO Vendas(id_venda, cliente, produto, data_venda, preco, quantidade) 
+comando = f"""INSERT INTO Vendas(id_venda, cliente, produto, data_venda, preco, quantidade) 
 VALUES
-    (4, 'camille', 'geladeira', '2025/08/11', 4500, 1),
-    (5, 'jorge', 'frigobar', '2025/08/15', 7000, 1)"""
+    ({id}, '{cliente}', '{produto}', '{data}', {preco}, {quantidade})"""
 
 cursor.execute(comando)
 cursor.commit()
